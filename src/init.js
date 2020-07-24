c8c4b49a8f9114318f7172c97129d548bdf58a60
Yann Depps <yann.deperrot@protonmail.com>
2020-07-24 16:18:17 +0200

    UI interactions

--- src/init.js
+++ src/init.js
@@ -1,9 +1,11 @@
-import game from "./gameState";
-
-const TICK_RATE = 3000;
+import game from './gameState';
+import { TICK_RATE } from './constants';
+import initButtons from './buttons';
 
 async function init() {
   console.log("starting game");
+
+  initButtons(game.handleUserAction);
 
   let nextTimeToTick = Date.now();
 
