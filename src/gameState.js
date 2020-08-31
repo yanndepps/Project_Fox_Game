10b1cb147abf372ee9533a95701fe4b17f0e5594
Yann Depps <yann.deperrot@protonmail.com>
2020-08-31 15:31:59 +0200

    refactoring

--- src/gameState.js
+++ src/gameState.js
@@ -6,6 +6,7 @@ import {
   NIGHT_LENGTH,
   getNextDieTime,
   getNextHungerTime,
+  getNextPoopTime,
 } from "./constants";
 
 const gameState = {
@@ -15,6 +16,8 @@ const gameState = {
   sleepTime: -1,
   hungryTime: -1,
   dieTime: -1,
+  timeToStartCelebrating: -1,
+  timeToEndCelebrating: -1,
   tick() {
     this.clock++;
     console.log("clock", this.clock);
@@ -26,6 +29,10 @@ const gameState = {
       this.getHungry();
     } else if (this.clock === this.dieTime) {
       this.die();
+    } else if (this.clock === this.timeToStartCelebrating) {
+      this.startCelebrating();
+    } else if (this.clock === this.timeToEndCelebrating) {
+      this.endCelebrating();
     }
     return this.clock;
   },
@@ -38,11 +45,11 @@ const gameState = {
   wake() {
     this.current = "IDLING";
     this.wakeTime = -1;
-    modFox("idling");
     this.scene = Math.random() > RAIN_CHANCE ? 0 : 1;
     modScene(SCENES[this.scene]);
     this.sleepTime = this.clock + DAY_LENGTH;
     this.hungryTime = getNextHungerTime(this.clock);
+    this.determineFoxState();
   },
   sleep() {
     this.state = "SLEEP";
@@ -58,6 +65,26 @@ const gameState = {
   },
   die() {
     console.log("die!");
+  },
+  startCelebrating() {
+    this.current = 'CELEBRATING';
+    modFox('celebrate');
+    this.timeToStartCelebrating = -1;
+    this.timeToEndCelebrating = this.clock + 2;
+  },
+  endCelebrating() {
+    this.timeToEndCelebrating = -1;
+    this.current = 'IDLING';
+    this.determineFoxState();
+  },
+  determineFoxState() {
+    if (this.current === 'IDLING') {
+      if (SCENES[this.scene] === 'rain') {
+        modFox('rain');
+      } else {
+        modFox('idling');
+      }
+    }
   },
   handleUserAction(icon) {
     // console.log(icon);
